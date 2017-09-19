8b7a62924a3cea9c51bba8e7a7b27cb12c67e19b
Dylan On <dylanon@users.noreply.github.com>
2017-09-19 12:51:51 -0400

    Set up child theme

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Music Picnic Theme' );
+define( 'CHILD_THEME_URL', 'https://dylanon.com' );
+define( 'CHILD_THEME_VERSION', '0.0.1' );
+
+?>
