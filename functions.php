8b68ca8040a7d9a77a5800485b825a1301d1cce7
Dylan On <dylanon@users.noreply.github.com>
2017-10-03 23:38:36 -0400

    Prelim work on theme and navbar, add normalize stylesheet and gitignore

--- functions.php
+++ functions.php
@@ -7,3 +7,22 @@ include_once( get_template_directory() . '/lib/init.php' );
 define( 'CHILD_THEME_NAME', 'Music Picnic Theme' );
 define( 'CHILD_THEME_URL', 'https://dylanon.com' );
 define( 'CHILD_THEME_VERSION', '0.0.1' );
+
+//* Add HTML5 markup structure for Genesis Framework
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+//* Remove the site description
+remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+
+// Enqueue fonts
+function mp_enqueue_styles() {
+	wp_enqueue_style( 'google-font-montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:400,900' );
+}
+add_action( 'wp_enqueue_scripts', 'mp_enqueue_styles' );
+
+//* Remove the header right widget area
+unregister_sidebar( 'header-right' );
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav', 12 );
