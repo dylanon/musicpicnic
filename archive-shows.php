a2abcaf0f41bfe4a583d872d11edf9c801d09c1a
Dylan On <dylanon@users.noreply.github.com>
2017-10-08 11:59:43 -0400

    Add custom loop to display shows post type listing

--- archive-shows.php
+++ archive-shows.php
@@ -2,5 +2,21 @@
 /**
  * Template Name: Shows Archive
  */
- 
- genesis();
+
+// First, standard genesis loop displays the page content.
+
+// Add a custom loop that displays posts of type 'shows'
+add_action( 'genesis_after_loop', 'mp_shows_archive_loop' );
+function mp_shows_archive_loop() {
+	genesis_markup( array(
+		'open'   => '<section %s>',
+		'context' => 'shows-listing',
+	) );
+	genesis_custom_loop( array( 'post_type'=>'shows' ) );
+	genesis_markup( array(
+		'close'   => '</section>',
+		'context' => 'shows-listing',
+	) );
+}
+
+genesis();
